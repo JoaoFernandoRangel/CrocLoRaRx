a057548c3a618c74778c3e48a8362b8d8fce504d
João Fernando Rangel <joaofrguimaraes1@gmail.com>
2024-12-14 17:57:02 -0300

    inicio dual core

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,58 @@
 #include <Arduino.h>
-#include <SPI.h>
 #include <LoRa.h>
+#include <SPI.h>
 // define the pins used by the transceiver module
 #define ss 5
 #define rst 14
 #define dio0 2
 
 int counter = 0;
-
-void setup()
-{
-  // initialize Serial Monitor
-  Serial.begin(115200);
-  Serial.println("LoRa Sender");
-
-  // setup LoRa transceiver module
-  LoRa.setPins(ss, rst, dio0);
-  while (!LoRa.begin(433E6))
-  {
-    Serial.println(".");
-    delay(500);
-  }
-  // Change sync word (0xF3) to match the receiver
-  // The sync word assures you don't get LoRa messages from other LoRa transceivers
-  // ranges from 0-0xFF
-  // LoRa.setSyncWord(0xF3);
-  Serial.println("LoRa Initializing OK!");
-}
-
 String LoRaData;
-void loop()
-{
-  // try to parse packet
-  int packetSize = LoRa.parsePacket();
-  if (packetSize)
-  {
-    // read packet
-    while (LoRa.available())
-    {
-      LoRaData = LoRa.readString();
-      Serial.print("Número recebido: ");
-      Serial.println(LoRaData);
+
+void LoRaTask(void *pvParameters);
+void ThingsBoardTask(void *pvParameters);
+void setup() {
+    // initialize Serial Monitor
+    Serial.begin(115200);
+
+    Serial.println("Init Setup");
+
+    // setup LoRa transceiver module
+    LoRa.setPins(ss, rst, dio0);
+    while (!LoRa.begin(433E6)) {
+        Serial.println(".");
+        delay(500);
     }
-    if ((LoRaData.toInt() % 2) == 1)
-    {
-      Serial.println("Número ímpar");
-    }
-    else
-    {
-      Serial.println("Número par");
-    }
-    LoRaData = "";
-  }
+    // Change sync word (0xF3) to match the receiver
+    // The sync word assures you don't get LoRa messages from other LoRa transceivers
+    // ranges from 0-0xFF
+    // LoRa.setSyncWord(0xF3);
+    Serial.println("LoRa Initializing OK!");
+    xTaskCreatePinnedToCore(LoRaTask, "thingsBoardTask", 10000, NULL, 1, NULL, 1);    // Executa no núcleo APP (Core 1)
+    xTaskCreatePinnedToCore(ThingsBoardTask, "autoOpTask", 10000, NULL, 1, NULL, 0);  // Executa no núcleo PRO (Core 0)
+    Serial.println("Finish Setup");
 }
+void loop() { vTaskSuspend(NULL); }
+void LoRaTask(void *pvParameters) {
+    while (1) {
+        // try to parse packet
+        int packetSize = LoRa.parsePacket();
+        if (packetSize) {
+            // read packet
+            while (LoRa.available()) {
+                LoRaData = LoRa.readString();
+                Serial.print("Número recebido: ");
+                Serial.println(LoRaData);
+            }
+            if ((LoRaData.toInt() % 2) == 1) {
+                Serial.println("Número ímpar");
+            } else {
+                Serial.println("Número par");
+            }
+            LoRaData = "";
+        }
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+}
+
+void ThingsBoardTask(void *pvParameters) {}
